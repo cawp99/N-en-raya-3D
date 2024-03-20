1feb758a88ee0efba17e032bf08232481eb27d8e
jr-isp <16-10553@usb.ve>
2024-03-19 20:29:58 -0400

    empecé el proyecto en consola

---
+++ proyecto-en-consola.py
@@ -0,0 +1,4 @@
+#Este será el archivo principal de una implementación del juego en 2D pero
+#en consola únicamente. El objetivo es que con este juego funcional,
+#se pueda simplemente pasar la parte grafica con tkinter como algo adicional
+#y modificable
